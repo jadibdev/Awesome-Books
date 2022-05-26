f4061e1347226d60a621f20f3b1ccd84aecbeccf
khan <khanetmoi@gmail.com>
2022-05-26 11:59:58 +0000

    Add navigation toggle functionality

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let storage = [];
 const submitButton = document.getElementById('submit');
 const booksContainer = document.getElementById('books-container');
 
+const formContainer = document.querySelector('.formContainer');
+const bookContainer = document.querySelector('#books-container');
+const contact = document.querySelector('.contact');
+const addNew = document.querySelector('#add_new');
+const ButtonContact = document.querySelector('#contact');
+const BookList = document.querySelector('#book_list');
+
 /* BOOK CLASS */
 class Book {
   constructor(title, author) {
@@ -29,6 +36,8 @@ class Book {
     const bookInfo = document.createElement('div');
     bookInfo.classList.add('bookInfo');
     bookCard.classList.add('bookCard');
+
+    bookContainer.classList.remove('disappear');
 
     const h2 = document.createElement('h2');
     h2.innerText = title;
@@ -65,43 +74,64 @@ class Book {
 }
 
 storage = JSON.parse(localStorage.getItem('books')) || [];
+if (storage.length > 0) {
+  for (let i = 0; i < storage.length; i += 1) {
+    const bookCard = document.createElement('div');
+    const bookInfo = document.createElement('div');
+    bookInfo.classList.add('bookInfo');
+    bookCard.classList.add('bookCard');
 
-for (let i = 0; i < storage.length; i += 1) {
-  const bookCard = document.createElement('div');
-  const bookInfo = document.createElement('div');
-  bookInfo.classList.add('bookInfo');
-  bookCard.classList.add('bookCard');
+    const h2 = document.createElement('h2');
+    h2.innerText = storage[i].title;
+    bookInfo.append(h2);
 
-  const h2 = document.createElement('h2');
-  h2.innerText = storage[i].title;
-  bookInfo.append(h2);
+    const h4 = document.createElement('h4');
+    h4.innerText = `by ${storage[i].author}`;
+    bookInfo.append(h4);
 
-  const h4 = document.createElement('h4');
-  h4.innerText = `by ${storage[i].author}`;
-  bookInfo.append(h4);
+    bookCard.append(bookInfo);
 
-  bookCard.append(bookInfo);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove';
+    bookCard.append(deleteButton);
 
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Remove';
-  bookCard.append(deleteButton);
+    const seperator = document.createElement('hr');
+    seperator.id = 'seperator';
 
-  const seperator = document.createElement('hr');
-  seperator.id = 'seperator';
+    booksContainer.append(bookCard);
+    booksContainer.append(seperator);
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
 
-  booksContainer.append(bookCard);
-  booksContainer.append(seperator);
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
+    /* REMOVE FUNCTION */
+    const removeButton = () => {
+      bookCard.remove();
+      localStorage.setItem('books', JSON.stringify(bookCollection));
+      seperator.parentNode.removeChild(seperator);
+    };
 
-  /* REMOVE FUNCTION */
-  const removeButton = () => {
-    bookCard.remove();
-    localStorage.setItem('books', JSON.stringify(bookCollection));
-    seperator.parentNode.removeChild(seperator);
-  };
-
-  deleteButton.addEventListener('click', removeButton);
+    deleteButton.addEventListener('click', removeButton);
+  }
+} else {
+  formContainer.classList.remove('disappear');
 }
 
 submitButton.addEventListener('click', Book.addButton);
+
+addNew.addEventListener('click', () => {
+  bookContainer.classList.add('disappear');
+  contact.classList.add('disappear');
+  formContainer.classList.remove('disappear');
+});
+
+ButtonContact.addEventListener('click', () => {
+  bookContainer.classList.add('disappear');
+  contact.classList.remove('disappear');
+  formContainer.classList.add('disappear');
+});
+
+BookList.addEventListener('click', () => {
+  bookContainer.classList.remove('disappear');
+  contact.classList.add('disappear');
+  formContainer.classList.add('disappear');
+});
